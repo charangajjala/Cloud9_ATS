80e0392a6fcecb55c06bd944ce0c93b9c8ad797c
Sachin625 <sachin.veeramalla@bekognitive.com>
2023-12-05 18:43:39 -0600

    Update DashEdit.js
    Can edit the form fields but cant submit. Not authorized to access this route error.

--- src/pages/admin/DashEdit.js
+++ src/pages/admin/DashEdit.js
@@ -1,50 +1,264 @@
-// export const DashEdit = () => {
-//     return <h1> This is edit page</h1>
+// // export const DashEdit = () => {
+// //     return <h1> This is edit page</h1>
+// // };
+
+
+// import { Box, MenuItem, Typography } from "@mui/material";
+// import React, { useEffect, useState } from "react";
+// import Axios from "axios"
+// import { useParams } from 'react-router-dom'
+// import TextField from "@mui/material/TextField";
+// import Button from "@mui/material/Button";
+// import { useFormik } from "formik";
+// import * as yup from "yup";
+// import { useDispatch, useSelector } from "react-redux";
+// import { jobTypeLoadAction } from "../../redux/actions/jobTypeAction";
+// import { registerAjobAction } from "../../redux/actions/jobAction";
+
+
+// const validationSchema = yup.object({
+//   title: yup.string("Enter a job title").required("title is required"),
+//   description: yup
+//     .string("Enter a description")
+//     .min(6, "Description should be of minimum 6 characters length")
+//     .required("Description is required"),
+//   salary: yup.number("Enter a salary").required("Salary is required"),
+//   location: yup.string("Enter a location").required("Location is required"),
+//   jobType: yup.string("Enter a Category").required("Category is required"),
+// });
+// const DashEdit = () => {
+// //   return <h1> This is a edit page </h1>
+//       const dispatch = useDispatch();
+//       const { id } = useParams();
+
+//   //job type
+//   useEffect(() => {
+//     dispatch(jobTypeLoadAction());
+//   }, [dispatch]);
+
+//   // useEffect(() => {
+//   //       Axios.get(`/api/job/${id}`).then(res =>
+//   //       setformik(res.data))
+//   //       .catch(err => console.log(err))
+
+//   // },[])
+
+//   const { jobType } = useSelector((state) => state.jobTypeAll);
+//   const [details, setDetails] = useState([])
+
+//   const formik = useFormik({
+//     initialValues: {
+//       title: "",
+//       description: "",
+//       salary: "",
+//       location: "",
+//       jobType: "",
+//     },
+//     enableReinitialize: true,
+//     // initialValues: {details},
+//     validationSchema: validationSchema,
+//     onSubmit: (values, actions) => {
+//       dispatch(registerAjobAction(values));
+//       // alert(JSON.stringify(values, null, 2));
+//       actions.resetForm();
+//     },
+//   });
+ 
+
+
+
+
+//     Axios.get(`/api/job/${id}`).then(res => {
+//         console.log('sachin', res.data)
+//         setDetails(res.data.job);
+        
+//         console.log(details)
+//         // console.log('test1', res.data.job)
+//         console.log('dif', formik.values)
+        
+
+//   });
+//   useEffect(() => {
+//       formik.setValues({
+//         ...details
+//       });
+//     }
+//   , [details]);
+//   return (
+//     <>
+//       <Box
+//         sx={{
+//           height: "100%",
+//           display: "flex",
+//           alignItems: "center",
+//           justifyContent: "center",
+//           pt: 4,
+//         }}
+//       >
+//         <Box
+//           onSubmit={formik.handleSubmit}
+//           component="form"
+//           className="form_style border-style"
+//         >
+//           <Box
+//             sx={{
+//               display: "flex",
+//               flexDirection: "column",
+//               alignItems: "center",
+//               width: "100%",
+//             }}
+//           >
+//             <Typography variant="h5" component="h2" sx={{ pb: 3 }}>
+//               Edit a Job
+//             </Typography>
+//             <TextField
+//               sx={{ mb: 3 }}
+//               fullWidth
+//               id="title"
+//               label="Title"
+//               name="title"
+//               InputLabelProps={{
+//                 shrink: true,
+//               }}
+//               // value={details.title}
+//               placeholder = "title"
+//               value={formik.values.title}
+
+//               onChange={formik.handleChange}
+//               onBlur={formik.handleBlur}
+//               error={formik.touched.title && Boolean(formik.errors.title)}
+//               helperText={formik.touched.title && formik.errors.title}
+//             />
+//             <TextField
+//               sx={{ mb: 3 }}
+//               fullWidth
+//               id="description"
+//               name="description"
+//               label="Description"
+//               type="text"
+//               InputLabelProps={{
+//                 shrink: true,
+//               }}
+//               placeholder="Description"
+//               value={formik.values.description}
+//               onChange={formik.handleChange}
+//               onBlur={formik.handleBlur}
+//               error={
+//                 formik.touched.description && Boolean(formik.errors.description)
+//               }
+//               helperText={
+//                 formik.touched.description && formik.errors.description
+//               }
+//             />
+//             <TextField
+//               sx={{ mb: 3 }}
+//               fullWidth
+//               id="salary"
+//               name="salary"
+//               label="Salary"
+//               type="text"
+//               InputLabelProps={{
+//                 shrink: true,
+//               }}
+//               placeholder="Salary"
+//               value={formik.values.salary}
+//               onChange={formik.handleChange}
+//               onBlur={formik.handleBlur}
+//               error={formik.touched.salary && Boolean(formik.errors.salary)}
+//               helperText={formik.touched.salary && formik.errors.salary}
+//             />
+//             <TextField
+//               sx={{ mb: 3 }}
+//               fullWidth
+//               id="location"
+//               name="location"
+//               label="Location"
+//               type="text"
+//               InputLabelProps={{
+//                 shrink: true,
+//               }}
+//               placeholder="Location"
+//               value={formik.values.location}
+//               onChange={formik.handleChange}
+//               onBlur={formik.handleBlur}
+//               error={formik.touched.location && Boolean(formik.errors.location)}
+//               helperText={formik.touched.location && formik.errors.location}
+//             />
+
+//             <TextField
+//               sx={{ mb: 3 }}
+//               fullWidth
+//               className="px-2 my-2"
+//               variant="outlined"
+//               name="jobType"
+//               id="jobType"
+//               select
+//               label="Category"
+//               value={formik.values.jobType}
+//               onChange={formik.handleChange}
+//               onBlur={formik.handleBlur}
+//               error={formik.touched.jobType && Boolean(formik.errors.jobType)}
+//               helperText={formik.touched.jobType && formik.errors.jobType}
+//             >
+//               <MenuItem key={""} value={""}></MenuItem>
+
+//               {jobType &&
+//                 jobType.map((cat) => (
+//                   <MenuItem key={cat.id} value={cat.id}>
+//                     {cat.jobTypeName}
+//                   </MenuItem>
+//                 ))}
+//             </TextField>
+
+//             <Button fullWidth variant="contained" type="submit">
+//               Edit job
+//             </Button>
+//           </Box>
+//         </Box>
+//       </Box>
+//     </>
+//   );
 // };
 
+// export default DashEdit;
 
-import { Box, MenuItem, Typography } from "@mui/material";
+
+
+
+
+
+import { Box, MenuItem, Typography, TextField, Button } from "@mui/material";
 import React, { useEffect, useState } from "react";
-import Axios from "axios"
-import { useParams } from 'react-router-dom'
-import TextField from "@mui/material/TextField";
-import Button from "@mui/material/Button";
+import Axios from "axios";
+import { useParams } from 'react-router-dom';
 import { useFormik } from "formik";
 import * as yup from "yup";
 import { useDispatch, useSelector } from "react-redux";
 import { jobTypeLoadAction } from "../../redux/actions/jobTypeAction";
 import { registerAjobAction } from "../../redux/actions/jobAction";
 
-
 const validationSchema = yup.object({
-  title: yup.string("Enter a job title").required("title is required"),
-  description: yup
-    .string("Enter a description")
-    .min(6, "Description should be of minimum 6 characters length")
-    .required("Description is required"),
+  title: yup.string("Enter a job title").required("Title is required"),
+  description: yup.string("Enter a description").min(6, "Description should be of minimum 6 characters length").required("Description is required"),
   salary: yup.number("Enter a salary").required("Salary is required"),
   location: yup.string("Enter a location").required("Location is required"),
   jobType: yup.string("Enter a Category").required("Category is required"),
 });
-const DashEdit = () => {
-//   return <h1> This is a edit page </h1>
-      const dispatch = useDispatch();
-      const { id } = useParams();
 
-  //job type
+const DashEdit = () => {
+  const dispatch = useDispatch();
+  const { id } = useParams();
+  const { jobType } = useSelector((state) => state.jobTypeAll);
+  const [details, setDetails] = useState(null);
+
   useEffect(() => {
     dispatch(jobTypeLoadAction());
-  }, [dispatch]);
-
-  // useEffect(() => {
-  //       Axios.get(`/api/job/${id}`).then(res =>
-  //       setformik(res.data))
-  //       .catch(err => console.log(err))
-
-  // },[])
-
-  const { jobType } = useSelector((state) => state.jobTypeAll);
-  const [details, setDetails] = useState([])
+    Axios.get(`/api/job/${id}`)
+      .then(res => {
+        setDetails(res.data.job);
+      })
+      .catch(err => console.log(err));
+  }, [dispatch, id]);
 
   const formik = useFormik({
     initialValues: {
@@ -55,35 +269,19 @@ const DashEdit = () => {
       jobType: "",
     },
     enableReinitialize: true,
-    // initialValues: {details},
     validationSchema: validationSchema,
     onSubmit: (values, actions) => {
       dispatch(registerAjobAction(values));
-      // alert(JSON.stringify(values, null, 2));
       actions.resetForm();
     },
   });
- 
 
-
-
-
-    Axios.get(`/api/job/${id}`).then(res => {
-        console.log('sachin', res.data)
-        setDetails(res.data.job);
-        
-        console.log(details)
-        // console.log('test1', res.data.job)
-        console.log('dif', formik.values)
-        
-
-  });
   useEffect(() => {
-      formik.setValues({
-        ...details
-      });
+    if (details) {
+      formik.setValues(details);
     }
-  , [details]);
+  }, [details]);
+
   return (
     <>
       <Box
@@ -111,24 +309,20 @@ const DashEdit = () => {
             <Typography variant="h5" component="h2" sx={{ pb: 3 }}>
               Edit a Job
             </Typography>
+
             <TextField
               sx={{ mb: 3 }}
               fullWidth
               id="title"
               label="Title"
               name="title"
-              InputLabelProps={{
-                shrink: true,
-              }}
-              // value={details.title}
-              placeholder = "title"
               value={formik.values.title}
-
               onChange={formik.handleChange}
               onBlur={formik.handleBlur}
               error={formik.touched.title && Boolean(formik.errors.title)}
               helperText={formik.touched.title && formik.errors.title}
             />
+
             <TextField
               sx={{ mb: 3 }}
               fullWidth
@@ -136,20 +330,13 @@ const DashEdit = () => {
               name="description"
               label="Description"
               type="text"
-              InputLabelProps={{
-                shrink: true,
-              }}
-              placeholder="Description"
               value={formik.values.description}
               onChange={formik.handleChange}
               onBlur={formik.handleBlur}
-              error={
-                formik.touched.description && Boolean(formik.errors.description)
-              }
-              helperText={
-                formik.touched.description && formik.errors.description
-              }
+              error={formik.touched.description && Boolean(formik.errors.description)}
+              helperText={formik.touched.description && formik.errors.description}
             />
+
             <TextField
               sx={{ mb: 3 }}
               fullWidth
@@ -157,16 +344,13 @@ const DashEdit = () => {
               name="salary"
               label="Salary"
               type="text"
-              InputLabelProps={{
-                shrink: true,
-              }}
-              placeholder="Salary"
               value={formik.values.salary}
               onChange={formik.handleChange}
               onBlur={formik.handleBlur}
               error={formik.touched.salary && Boolean(formik.errors.salary)}
               helperText={formik.touched.salary && formik.errors.salary}
             />
+
             <TextField
               sx={{ mb: 3 }}
               fullWidth
@@ -174,10 +358,6 @@ const DashEdit = () => {
               name="location"
               label="Location"
               type="text"
-              InputLabelProps={{
-                shrink: true,
-              }}
-              placeholder="Location"
               value={formik.values.location}
               onChange={formik.handleChange}
               onBlur={formik.handleBlur}
@@ -188,8 +368,6 @@ const DashEdit = () => {
             <TextField
               sx={{ mb: 3 }}
               fullWidth
-              className="px-2 my-2"
-              variant="outlined"
               name="jobType"
               id="jobType"
               select
@@ -200,18 +378,15 @@ const DashEdit = () => {
               error={formik.touched.jobType && Boolean(formik.errors.jobType)}
               helperText={formik.touched.jobType && formik.errors.jobType}
             >
-              <MenuItem key={""} value={""}></MenuItem>
-
-              {jobType &&
-                jobType.map((cat) => (
-                  <MenuItem key={cat.id} value={cat.id}>
-                    {cat.jobTypeName}
-                  </MenuItem>
-                ))}
+              {jobType && jobType.map((cat) => (
+                <MenuItem key={cat.id} value={cat.id}>
+                  {cat.jobTypeName}
+                </MenuItem>
+              ))}
             </TextField>
 
             <Button fullWidth variant="contained" type="submit">
-              Edit job
+              Edit Job
             </Button>
           </Box>
         </Box>
@@ -221,6 +396,7 @@ const DashEdit = () => {
 };
 
 export default DashEdit;
+
 
 
 
